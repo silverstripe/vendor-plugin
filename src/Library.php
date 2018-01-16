1e56267316e3364b4aa6a2fa5215d0f1c3398695
Damian Mooyman <damian@silverstripe.com>
2018-01-17 10:25:01 +1300

    BUG Fix symlinks failing on non-vendor modules Fixes https://github.com/silverstripe/silverstripe-framework/issues/7765

--- src/Library.php
+++ src/Library.php
@@ -46,8 +46,8 @@ class Library
      */
     public function __construct($basePath, $libraryPath, $name = null)
     {
-        $this->basePath = $basePath;
-        $this->path = $libraryPath;
+        $this->basePath = realpath($basePath);
+        $this->path = realpath($libraryPath);
         $this->name = $name;
     }
 
