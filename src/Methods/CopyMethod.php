d1f1ceab7b47713a04e57ebd7ecc38e35da6d4af
Guy Sartorelli <36352093+GuySartorelli@users.noreply.github.com>
2025-01-07 17:26:12 +1300

    API Remove deprecated API (#88)
    Note that we will also need to update any composer references to
    vendor-plugin to use the new version, since until this we were just
    going to keep using 2.x

--- src/Methods/CopyMethod.php
+++ src/Methods/CopyMethod.php
@@ -34,34 +34,4 @@ class CopyMethod implements ExposeMethod
             throw new RuntimeException("Could not write to directory $target");
         }
     }
-
-    /**
-     * Copies a file or directory from $source to $target.
-     *
-     *
-     * @param string $source
-     * @param string $target
-     * @deprecated 5.2 Use Filesystem::copy instead
-     * @return bool
-     */
-    public function copy($source, $target)
-    {
-        if (!is_dir($source ?? '')) {
-            return copy($source ?? '', $target ?? '');
-        }
-        $it = new RecursiveDirectoryIterator($source, RecursiveDirectoryIterator::SKIP_DOTS);
-        /** @var RecursiveDirectoryIterator $ri */
-        $ri = new RecursiveIteratorIterator($it, RecursiveIteratorIterator::SELF_FIRST);
-        $this->filesystem->ensureDirectoryExists($target);
-        $result = true;
-        foreach ($ri as $file) {
-            $targetPath = $target . DIRECTORY_SEPARATOR . $ri->getSubPathName();
-            if ($file->isDir()) {
-                $this->filesystem->ensureDirectoryExists($targetPath);
-            } else {
-                $result = $result && copy($file->getPathname() ?? '', $targetPath ?? '');
-            }
-        }
-        return $result;
-    }
 }
